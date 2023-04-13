6dbafa24fa8b3e5a252a18942884fc2107e98ac6
kubraozyakisir <kbrozyksr@gmail.com>
2023-04-13 23:49:19 +0300

    Token created
    Token created

---
+++ ExampleProject/ExampleProject/Security/Token.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleProject.Security
+{
+    public class Token
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime Expiration { get; set; }
+     }
+}
